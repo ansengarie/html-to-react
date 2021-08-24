a0861411cbd32e0546f99a61fba6b5945a891e50
ajins15 <ansengarie@gmail.com>
2021-08-24 13:58:54 +0700

    add header

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -1,5 +1,27 @@
 import React from "react";
 
-export default function Button() {
-  return <div>Button</div>;
+import propTypes from "prop-types";
+
+export default function Button(props) {
+  const className = ["button"];
+  if (props.isPrimary) {
+    className.push("button-primary");
+  }
+  if (props.isWideMobile) {
+    className.push("button-wide-mobile");
+  }
+  if (props.isSmall) {
+    className.push("button-sm");
+  }
+  return (
+    <button className={`button ${className.join(" ")}`}>
+      {props.children}
+    </button>
+  );
 }
+
+Button.propTypes = {
+  isPrimary: propTypes.bool,
+  isWideMobile: propTypes.bool,
+  isSmall: propTypes.bool,
+};
--- src/scenes/Homepage.js
+++ src/scenes/Homepage.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-import Button from "../elements/Button";
+import Header from "../parts/Header";
 
 export default function Homepage() {
   return (
     <div>
-      <Button>Sample</Button>
+      <Header></Header>
     </div>
   );
 }
